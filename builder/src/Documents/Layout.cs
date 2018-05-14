50e619634ad8c7319b2c290bce29da67adae4225
Erik Faye-Lund <kusmabite@gmail.com>
2018-05-14 14:29:50 +0200

    generator: support expanding ##BASEURL##

--- builder/src/Documents/Layout.cs
+++ builder/src/Documents/Layout.cs
@@ -7,10 +7,12 @@ namespace Builder.Documents
     public class Layout
     {
         private readonly string _template;
+        private readonly string _baseUrl;
 
         public Layout(BuilderSettings settings)
         {
             _template = ReadTemplate(settings.RootPath);
+            _baseUrl = settings.BaseUrl;
         }
 
         public string Apply(string html, string sidebar, string title)
@@ -24,7 +26,8 @@ namespace Builder.Documents
             html = _template.Replace("##BODY##", html)
                             .Replace("##SIDEBAR##", sidebar)
                             .Replace("##PAGETITLE##", HtmlEncoder.Default.Encode(fullTitle))
-                            .Replace("##TITLE##", HtmlEncoder.Default.Encode(title));
+                            .Replace("##TITLE##", HtmlEncoder.Default.Encode(title))
+                            .Replace("##BASEURL##", HtmlEncoder.Default.Encode(_baseUrl));
             return html;
         }
 
